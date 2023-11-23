b29a22bc75f03f853a82fe4fed2cfbf765aa0d1d
MAXX98 <dahakar1998@gmail.com>
2023-11-23 17:27:04 -0400

    sync

---
+++ index.mjs
@@ -0,0 +1,128 @@
+import {
+  makeWASocket,
+  DisconnectReason,
+  useMultiFileAuthState,
+} from "@whiskeysockets/baileys";
+
+import { readFileSync } from "fs";
+import log from "pino";
+import { Boom } from "@hapi/boom";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import urlToTemplateAndMedia from "./templatesUrls.mjs";
+import { city } from "./templates.mjs";
+
+const { json, urlencoded } = bodyParser;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+let sock;
+let qrDinamic;
+let soket;
+let count = 0;
+
+const actions = {
+  [DisconnectReason.badSession]: () => {
+    console.log(`Bad Session File, Delete session_auth_info and Scan Again`);
+    sock.logout();
+  },
+  [DisconnectReason.connectionClosed]: reconnect,
+  [DisconnectReason.connectionLost]: reconnect,
+  [DisconnectReason.connectionReplaced]: () => {
+    console.log("Conexión con otra sesión abierta, cierre la sesión actual");
+    sock.logout();
+  },
+  [DisconnectReason.loggedOut]: () => {
+    console.log(`Dispositivo cerrado, eliminar session_auth_info y escanear`);
+    sock.logout();
+  },
+  [DisconnectReason.restartRequired]: reconnect,
+  [DisconnectReason.timedOut]: reconnect,
+};
+
+async function connectToWhatsApp() {
+  const { state, saveCreds } = await useMultiFileAuthState("session_auth_info");
+
+  sock = makeWASocket({
+    printQRInTerminal: true,
+    auth: state,
+    logger: log({ level: "silent" }),
+  });
+
+  sock.ev.on("connection.update", handleConnectionUpdate);
+  sock.ev.on("messages.upsert", handleMessageUpsert);
+  sock.ev.on("creds.update", saveCreds);
+}
+
+function handleConnectionUpdate(update) {
+  const { connection, lastDisconnect, qr } = update;
+  qrDinamic = qr;
+  if (connection === "close") {
+    let reason = new Boom(lastDisconnect.error).output.statusCode;
+    actions[reason]?.();
+  } else if (connection === "open") {
+    console.log("conexión abierta");
+  }
+}
+
+async function handleMessageUpsert({ messages, type }) {
+  try {
+    if (type === "notify" && !messages[0]?.key.fromMe) {
+      const sourceUrl =
+        messages[0]?.message?.extendedTextMessage?.contextInfo?.externalAdReply
+          ?.sourceUrl || "";
+      const messageBody = messages[0].message.extendedTextMessage.text;
+      const clientNumber = messages[0]?.key?.remoteJid;
+
+      const templateAndMedia =
+        urlToTemplateAndMedia[messageBody] || urlToTemplateAndMedia[sourceUrl];
+
+      if (templateAndMedia) {
+        console.log(
+          `mensaje enviado ${++count} ${new Date().toLocaleTimeString()}`
+        );
+        console.log(clientNumber);
+        //const image = readFileSync(templateAndMedia.media);
+        const image = templateAndMedia.media;
+        await sock.sendMessage(clientNumber, {
+          image: image,
+          caption: templateAndMedia.template,
+        });
+
+        setTimeout(async () => {
+          await sock.sendMessage(clientNumber, {
+            text: city,
+          });
+        }, 2000);
+      }
+    }
+  } catch (error) {
+    console.log("error ", error);
+  }
+}
+
+function reconnect() {
+  console.log("Reconectando...");
+  connectToWhatsApp();
+}
+
+io.on("connection", async (socket) => {
+  soket = socket;
+  if (sock?.user) {
+    updateQR("connected");
+  } else if (qrDinamic) {
+    updateQR("qr");
+  }
+});
+
+connectToWhatsApp().catch((err) => console.log("unexpected error: " + err));
+server.listen(port, () => {});
---
+++ templates.mjs
@@ -0,0 +1,118 @@
+export const zl02pro = `Nos preguntaste por el SMARTWATCH ZL02 PRO⌚
+  ──────────────────
+  Principales características 🧏🏽‍♂️
+  ·٠•● Recibe notificaciones de WhatsApp, Face y otros
+  ·٠•● Contesta y realiza llamadas
+  ·٠•● Controla canciones (compatible con audífonos Bluetooth)
+  ·٠•● Medidor pulso cardíaco, oxímetro, presión sanguínea y respiración
+  ·٠•● Carga Magnética
+  ──────────────────
+  Características TÉCNICAS📲
+  ·٠•● Batería de 200 mAh (Dura 5-6 dias)
+  ·٠•● Manillas de silicona suave
+  ·٠•● Pantalla Touch de Alta Definición 240*240 (super nítido )
+  ·٠•● Pantalla de 1.39 pulgadas
+  ·٠•● Hasta 60 fondos de pantalla
+  ·٠•● A prueba de sudor y salpicaduras IP67 (sumergible por media hora)
+  ──────────────────
+  Disponibilidad 🎨
+  ◉ Color: Negro, Azul, Rosado, Beige
+  ◉ *Precio: Bs. 250*`;
+export const lenovok3 = `*Nos preguntaste por el parlante Lenovo RGB*
+  ──────────────
+  *Principales características*
+  - Diseño portátil para llevar a todas partes
+  - Micrófono interno para contestar llamadas
+  - Volumen potente
+  - 4 modos de iluminación (incluye modo que reacciona con la música)
+  ──────────────
+  *Características técnicas*
+  - Conexión Bluetooth 5.2
+  - Batería de 500mAh
+  - Modo sincronización de 2 parlantes
+  ──────────────
+  *Precio Bs.130 c/u*
+  *Combo de 2 unidades por Bs. 199*`;
+export const comboPROQT82 = `*Combo Smartwatch Deportivo ⌚+ Audífonos inalámbricos Lenovo Premium*
+  -------------------------------
+  
+  *SmartWatch Sumergible*
+  Pantalla de 1.39 pulgadas de alta resolución
+  Contesta y realiza llamadas
+  Cuenta con medidores de salud
+  Recibe notificaciones de tus redes sociales preferidas
+  Controla tus canciones ¡Incluyendo audífonos Bluetooth!
+  *Audífonos Lenovo QT82*
+  Batería de larga duración de hasta 12 horas.
+  Conexión inalámbrica Bluetooth 5.0
+  Cable de carga Tipo C
+  Estuche de carga compacto de 300mAh
+  -----------------------------------
+  P̶r̶e̶c̶i̶o̶ ̶p̶o̶r̶ ̶s̶e̶p̶a̶r̶a̶d̶o̶ ̶B̶s̶.̶ ̶4̶1̶0̶
+  Precio en combo Bs. 329`;
+export const rechargeBattery = `*Nos preguntaste las Pilas Recargables por USB*
+  ──────────────────
+  *Principales características* 🧏🏽
+  ·٠•● Pack de 4 pilas tamaño AA (1700mAh)
+  ·٠•● Cable de carga USB con  2 puertos para comodidad
+  ·٠•● Pueden cargarse en cualquier dispositivo o directamente en la corriente
+  ──────────────────
+  *Características TÉCNICAS*📲
+  ·٠•● 1,5 horas para carga al 100%
+  ·٠•● Vida útil hasta 1200 recargas
+  ·٠•● Carga directa con cable USB-C 2 en 1 incluido
+  ──────────────────
+  *Presentaciones*
+  ◉ Pack de 4 unidades pilas AA con cable USB-C de 2 puertos
+  ◉ Pack de 4 unidades pilas AAA con cable USB-C de 2 puertos
+  ◉ Pack de 2 unidades pilas AA y 2 unidades pilas AAA con cable USB-C de 2 puertos
+  ◉ *~Precio Bs. 150~
+  ◉ *Precio de oferta cualquiera de las presentaciones: Bs. 135*`;
+export const deskLamp = `Nos preguntaste por la Lampara de Escritorio 💡
+  ──────────────────
+  Principales características 🧏🏽‍♂️
+  ·٠•● 5 niveles de temperatura (blanco a cálido)
+  ·٠•● 5 niveles de brillo
+  ·٠•● Alfombrilla antideslizante
+  ·٠•● Temporizador
+  ──────────────────
+  Características TÉCNICAS📲
+  ·٠•● Giro horizontal 160 grados
+  ·٠•● Se puede plegar y estirar en 225 grados
+  ·٠•● Puerto de carga USB
+  ──────────────────
+  Disponibilidad 🎨
+  ◉ color: Plomo
+  ◉ *Precio: Bs. 180* `;
+export const proyector = `Nos preguntaste por el Proyector Portátil📽️
+  ──────────────────
+  Principales características 🧏🏽‍♂️
+  ·٠•● Ligera y compacta
+  ·٠•● Altavoces incorporados
+  ·٠•● Distancia de proyección recomendada 2 metros (60 pulgadas)
+  ·٠•● Recomendado para ver películas y videos
+  ──────────────────
+  Características TÉCNICAS🛠️
+  ·٠•● Interfaz compatible con USB/RCA/HDMI/SD
+  ·٠•● Tamaño: 12.7 cm x 8 cm  x 4.5 cm
+  ·٠•● Brillo 400-800 lúmenes,
+  ·٠•● definición 320x240px
+  ·٠•● Permite la conexión de Chromecast, Tv Stick, etc.
+  ──────────────────
+  ◉ *Precio: Bs. 300* `;
+export const galaxyLamp = `Nos preguntaste por el Galaxy Lamp🌌
+  ──────────────────
+  Principales características 🧏🏽‍♂️
+  ·٠•● Tecnología LED avanzada
+  ·٠•● Controla desde la App
+  ·٠•● Temperatura de color ajustable
+  ·٠•● Altavoz Bluetooth integrado
+  ──────────────────
+  Características TÉCNICAS🛠️
+  ·٠•● Amplio ángulo de haz (120°)
+  ·٠•● Tamaño: 16.6 cm x 16.6 cm x 13 cm
+  ·٠•● 1 mando a la distancia.
+  ──────────────────
+  ◉ *~Precio Bs. 180~
+  *Precio de oferta: Bs. 159* `;
+export const city = `¿Desde qué ciudad nos escribe? 🇧🇴😁`;
