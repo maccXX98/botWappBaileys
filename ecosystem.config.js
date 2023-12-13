3be9cbc6c2eed316df923b8cda7057ab262b49d8
MAXX98 <dahakar1998@gmail.com>
2023-12-13 15:53:58 -0400

    delete dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,13 @@
 module.exports = {
   apps: [
     {
-      script: "server.js",
-      env: {
-        NODE_ENV: "develoment",
-        PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
-        PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
-      },
-    },
-    {
+      name: "botWAPP",
       script: "server.js",
       env_production: {
         NODE_ENV: "production",
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
       },
-    },
-    {
-      name: "botWAPP",
-      script: "server.js",
     },
   ],
 };
